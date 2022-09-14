f1fdc3525e69cb2d75815cd007c9f1bab7ff3e6f
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-09-14 22:54:59 +0200

    Improve TS config

--- lunarvim/.config/lvim/after/ftplugin/typescript.lua
+++ lunarvim/.config/lvim/after/ftplugin/typescript.lua
@@ -1,21 +1,21 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  -- { name = 'eslint_d' },
+  { name = 'eslint_d' },
   { name = 'prettierd' },
   -- { name = "eslint" },
   -- { name = "prettier" }
 }
 
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
--- { name = 'eslint_d' }
--- { name = "eslint" }
--- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { name = 'eslint_d' }
+  -- { name = "eslint" }
+}
 
 local code_actions = require "lvim.lsp.null-ls.code_actions"
 code_actions.setup {
-  { name = "refactoring" }
-  -- { name = 'eslint_d' }
+  { name = "refactoring" },
+  { name = 'eslint_d' },
   -- { name = "eslint" }
 }
 
@@ -23,8 +23,16 @@ require 'lspconfig'.graphql.setup {
   filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" }
 }
 
-require 'lspconfig'.eslint.setup {
-  format = true
+-- require 'lspconfig'.eslint.setup {
+--   format = true
+-- }
+
+require 'lspconfig'.graphql.setup {
+  filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
+  cmd = {
+    "/Users/jonatanwitoszek/git-clone/graphiql/packages/graphql-language-service-cli/bin/graphql.js", "server", "-m",
+    "stream"
+  }
 }
 
 -- require 'lspconfig'.tailwindcss.setup {}
