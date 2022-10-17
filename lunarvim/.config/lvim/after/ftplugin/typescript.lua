6eae06761b79ff4a2e8754628d87078fff2ed2b2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-10-17 13:37:01 +0200

    Update vim config

--- lunarvim/.config/lvim/after/ftplugin/typescript.lua
+++ lunarvim/.config/lvim/after/ftplugin/typescript.lua
@@ -23,16 +23,26 @@ require 'lspconfig'.graphql.setup {
   filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" }
 }
 
+-- require 'lspconfig'.denols.setup {}
+
+local util = require 'lspconfig.util'
+require 'lspconfig'.tsserver.setup {
+  root_dir = function(fname)
+    return util.root_pattern 'tsconfig.json' (fname)
+        or util.root_pattern('package.json', 'jsconfig.json')(fname)
+  end,
+}
+
 -- require 'lspconfig'.eslint.setup {
 --   format = true
 -- }
 
-require 'lspconfig'.graphql.setup {
-  filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
-  cmd = {
-    "/Users/jonatanwitoszek/git-clone/graphiql/packages/graphql-language-service-cli/bin/graphql.js", "server", "-m",
-    "stream"
-  }
-}
+-- require 'lspconfig'.graphql.setup {
+--   filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
+--   cmd = {
+--     "/Users/jonatanwitoszek/git-clone/graphiql/packages/graphql-language-service-cli/bin/graphql.js", "server", "-m",
+--     "stream"
+--   }
+-- }
 
 -- require 'lspconfig'.tailwindcss.setup {}
---
+++ lunarvim/.config/lvim/ftdetect/env.lua
@@ -0,0 +1,6 @@
+vim.filetype.add({
+  filename = {
+    [".env.local"] = "sh",
+    [".env.development"] = "sh"
+  }
+})
